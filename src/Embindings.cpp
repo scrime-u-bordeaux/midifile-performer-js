b5cf39bbc1cbbdf7f31ef9300b01adfea9403c13
Joseph Larralde <joseph.larralde@gmail.com>
2022-08-29 09:20:32 +0200

    adapted to feat/iterable-chronology branch

--- src/Embindings.cpp
+++ src/Embindings.cpp
@@ -1,15 +1,67 @@
 #include <emscripten/bind.h>
-#include "libMidifilePerformer/src/include/impl/MFPRenderer.h"
+#include "libMidifilePerformer/src/include/impl/SequencePerformer.h"
 
 using namespace emscripten;
 
+// int64_t is not supported by emscripten so we need these utilities
+
+// correct found here :
+// https://github.com/emscripten-core/emscripten/issues/6492
+// getters / setters can also be specified with optional_override(lambda), see :
+// https://intrepidgeeks.com/tutorial/wasm-emcriptenembind-int64nknown-type-x
+
+template <typename T>
+double eventset_getdt(const Events::Set<T>& s) {
+  return static_cast<double>(s.dt);
+};
+
+template <typename T>
+void eventset_setdt(Events::Set<T>& s, double dt) {
+  s.dt = static_cast<std::int64_t>(dt);
+};
+
+// specializations :
+
+static double noteset_getdt(const Events::Set<noteData>& s) {
+  return eventset_getdt<noteData>(s);
+}
+
+static void noteset_setdt(Events::Set<noteData>& s, double dt) {
+  return eventset_setdt<noteData>(s, dt);
+}
+
+static double commandset_getdt(const Events::Set<commandData>& s) {
+  return eventset_getdt<commandData>(s);
+}
+
+static void commandset_setdt(Events::Set<commandData>& s, double dt) {
+  return eventset_setdt<commandData>(s, dt);
+}
+
 EMSCRIPTEN_BINDINGS(MidifilePerformer) {
+
+  // NOTE STRUCTS //////////////////////////////////////////////////////////////
+
   value_object<noteData>("noteData")
     .field("on",      &noteData::on)
     .field("pitch",   &noteData::pitch)
     .field("velocity",&noteData::velocity)
     .field("channel", &noteData::channel)
     ;
+
+  register_vector<noteData>("noteVector");
+
+  value_object<Events::Set<noteData>>("noteSet")
+    .field("dt",      &noteset_getdt, &noteset_setdt)
+    .field("events",  &Events::Set<noteData>::events)
+    ;
+
+  value_object<Events::SetPair<noteData>>("noteSetPair")
+    .field("start",   &Events::SetPair<noteData>::start)
+    .field("end",     &Events::SetPair<noteData>::end)
+    ;
+
+  // COMMAND STRUCTS ///////////////////////////////////////////////////////////
 
   value_object<commandData>("commandData")
     .field("pressed", &commandData::pressed)
@@ -18,7 +70,19 @@ EMSCRIPTEN_BINDINGS(MidifilePerformer) {
     .field("channel", &commandData::channel)
     ;
 
-  register_vector<noteData>("noteDataSet");
+  register_vector<commandData>("commandVector");
+
+  value_object<Events::Set<commandData>>("commandSet")
+    .field("dt",      &commandset_getdt, &commandset_setdt)
+    .field("events",  &Events::Set<commandData>::events)
+    ;
+
+  value_object<Events::SetPair<commandData>>("commandSetPair")
+    .field("start",   &Events::SetPair<commandData>::start)
+    .field("end",     &Events::SetPair<commandData>::end)
+    ;
+
+  // CHRONOLOGY OPTIONS AND PARAMETERS /////////////////////////////////////////
 
   typedef Events::correspondOption ShiftMode;
 
@@ -37,42 +101,61 @@ EMSCRIPTEN_BINDINGS(MidifilePerformer) {
     .field("temporalResolution",  &ChronologyParameters::temporalResolution)
     ;
 
-  class_<Chronology<noteData>>("Chronology")
+  // CHRONOLOGY ////////////////////////////////////////////////////////////////
+ 
+  typedef Chronology<noteData, std::vector> Chronology;
+
+  class_<Chronology>("Chronology")
     .constructor()
-    .function("pushEvent",    &Chronology<noteData>::pushEvent)
-    .function("finalize",     &Chronology<noteData>::finalize)
-    .function("hasEvents",    &Chronology<noteData>::hasEvents)
-    .function("pullEvents",   &Chronology<noteData>::pullEvents)
-    .function("clear",        &Chronology<noteData>::clear)
-    .function("size",         &Chronology<noteData>::size)
+    .function("size",       &Chronology::size)
+    .function("clear",      &Chronology::clear)
+    .function("pushEvent",  &Chronology::pushEvent)
+    .function("finalize",   &Chronology::finalize)
     ;
 
-  typedef ChordVelocityMapping::StrategyType ChordStrategy;
+  // PERFORMER STRATEGIES //////////////////////////////////////////////////////
+
+  typedef ChordRendering::StrategyType ChordStrategy;
 
   enum_<ChordStrategy>("chordStrategy")
+    .value("none",                    ChordStrategy::None)
     .value("sameForAll",              ChordStrategy::SameForAll)
     .value("clippedScaledFromMean",   ChordStrategy::ClippedScaledFromMean)
     .value("adjustedScaledFromMean",  ChordStrategy::AdjustedScaledFromMean)
     .value("clippedScaledFromMax",    ChordStrategy::ClippedScaledFromMax)
     ;
 
-  typedef MFPRenderer Renderer;
-  // typedef MFPRenderer<noteData, commandData, commandKey> Renderer;
-  // typedef Renderer<noteData, commandData, commandKey> Renderer;
+  typedef VoiceStealing::StrategyType VoiceStealingStrategy;
 
-  class_<Renderer>("Renderer")
+  enum_<VoiceStealingStrategy>("voiceStealingStrategy")
+    .value("none",            VoiceStealingStrategy::None)
+    .value("lastNoteOffWins", VoiceStealingStrategy::LastNoteOffWins)
+    .value("onlyStaccato",    VoiceStealingStrategy::OnlyStaccato)
+    ;
+
+  // PERFORMER /////////////////////////////////////////////////////////////////
+
+  typedef SequencePerformer Performer;
+
+  class_<AbstractPerformer>("AbstractPerformer")
+    .function(
+      "setVoiceStealingStrategy",
+      &AbstractPerformer::setVoiceStealingStrategy
+    )
+    .function(
+      "setChordRenderingStrategy",
+      &AbstractPerformer::setChordRenderingStrategy
+    )
+    ;
+  class_<Performer, base<AbstractPerformer>>("Performer")
     .constructor<ChronologyParameters>()
-    .function("setChordRenderingStrategy",&Renderer::setChordRenderingStrategy)
-    .function("pushEvent",                &Renderer::pushEvent)
-    .function("finalize",                 &Renderer::finalize)
-    .function("hasEvents",                &Renderer::hasEvents)
-    .function("pullEvents",               &Renderer::pullEvents)
-    .function("pullEventsSet",            &Renderer::pullEventsSet)
-    .function("combine3",                 &Renderer::combine3)
-    // .function("combine3",
-    //   select_overload<std::vector<noteData>(commandData, bool)>(&Renderer::combine3))
-    .function("clear",                    &Renderer::clear)
-    .function("setPartition",             &Renderer::setPartition)
-    .function("getPartition",             &Renderer::getPartition)
+    .function("size",                     &Performer::size)
+    .function("clear",                    &Performer::clear)
+    .function("pushEvent",                &Performer::pushEvent)
+    .function("finalize",                 &Performer::finalize)
+    .function("render",
+      select_overload<std::vector<noteData>(commandData)>(&Performer::render))
+    .function("setChronology",            &Performer::setChronology)
+    .function("getChronology",            &Performer::getChronology)
     ;
 }
--- test/basic-cases.js
+++ test/basic-cases.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const MidifilePerformer = require('../dist/node/MidifilePerformer.js');
 
-/* * * * * * * * * * * * * * * * * UTILITIES * * * * * * * * * * * * * * * * */
+// UTILITIES ///////////////////////////////////////////////////////////////////
 
 const DEF_ON_VELOCITY = 127
 const DEF_OFF_VELOCITY = 0;
@@ -19,208 +19,233 @@ function command(pressed, id, velocity = DEF_ON_VELOCITY, channel = DEF_CHANNEL)
     return { pressed, id, velocity, channel };
 }
 
-////////// scores
+// SCORES //////////////////////////////////////////////////////////////////////
 
 const before = [
-    [ 0, true,  60 ],
-    [ 5, false, 60 ],
-    [ 5, true,  62 ],
-    [ 5, false, 62 ]
+  [ 0, true,  60 ],
+  [ 5, false, 60 ],
+  [ 5, true,  62 ],
+  [ 5, false, 62 ]
 ];
 
 const meets = [
-    [ 0, true,  60 ],
-    [ 5, false, 60 ],
-    [ 0, true,  62 ],
-    [ 5, false, 62 ]
+  [ 0, true,  60 ],
+  [ 5, false, 60 ],
+  [ 0, true,  62 ],
+  [ 5, false, 62 ]
 ];
 
 const overlaps = [
-    [ 0, true,  60 ],
-    [ 5, true,  62 ],
-    [ 5, false, 60 ],
-    [ 5, false, 62 ]
+  [ 0, true,  60 ],
+  [ 5, true,  62 ],
+  [ 5, false, 60 ],
+  [ 5, false, 62 ]
 ];
 
 const starts = [
-    [ 0, true,  60 ],
-    [ 0, true,  62 ],
-    [ 5, false, 60 ],
-    [ 5, false, 62 ]
+  [ 0, true,  60 ],
+  [ 0, true,  62 ],
+  [ 5, false, 60 ],
+  [ 5, false, 62 ]
 ];
 
 const during = [
-    [ 0, true,  60 ],
-    [ 5, true,  62 ],
-    [ 5, false, 62 ],
-    [ 5, false, 60 ]
+  [ 0, true,  60 ],
+  [ 5, true,  62 ],
+  [ 5, false, 62 ],
+  [ 5, false, 60 ]
 ];
 
 const finishes = [
-    [ 0, true,  60 ],
-    [ 5, true,  62 ],
-    [ 5, false, 60 ],
-    [ 0, false, 62 ]
+  [ 0, true,  60 ],
+  [ 5, true,  62 ],
+  [ 5, false, 60 ],
+  [ 0, false, 62 ]
 ];
 
 const equals = [
-    [ 0, true,  60 ],
-    [ 0, true,  62 ],
-    [ 5, false, 60 ],
-    [ 0, false, 62 ]
+  [ 0, true,  60 ],
+  [ 0, true,  62 ],
+  [ 5, false, 60 ],
+  [ 0, false, 62 ]
 ];
 
 const normalScore = [
-    [ 0, true, 60 ],
-    [ 0, true, 64 ],
-    [ 0, true, 67 ],
-    [ 0, true, 68 ],
-    [ 5, false, 67 ],
-    [ 5, false, 64 ],
-    [ 5, false, 60 ],
-    [ 5, false, 68 ],
+  [ 0, true, 60 ],
+  [ 0, true, 64 ],
+  [ 0, true, 67 ],
+  [ 0, true, 68 ],
+  [ 5, false, 67 ],
+  [ 5, false, 64 ],
+  [ 5, false, 60 ],
+  [ 5, false, 68 ],
 ];
 
-////////// commands
+// COMMANDS ////////////////////////////////////////////////////////////////////
 
 const beforeCmd = [
-    [ true,  60 ],
-    [ false, 60 ],
-    [ true,  62 ],
-    [ false, 62 ]
+  [ true,  60 ],
+  [ false, 60 ],
+  [ true,  62 ],
+  [ false, 62 ]
 ];
 
 const overlapsCmd = [
-    [ true,  60 ],
-    [ true,  62 ],
-    [ false, 60 ],
-    [ false, 62 ]
+  [ true,  60 ],
+  [ true,  62 ],
+  [ false, 60 ],
+  [ false, 62 ]
 ];
 
 const duringCmd = [
-    [ true,  60 ],
-    [ true,  62 ],
-    [ false, 62 ],
-    [ false, 60 ]
+  [ true,  60 ],
+  [ true,  62 ],
+  [ false, 62 ],
+  [ false, 60 ]
 ];
 
 const longCommandSeries = [
-    [ true, 60 ],
-    [ false, 60 ],
-    [ true, 62 ],
-    [ false, 62 ],
+  [ true, 60 ],
+  [ false, 60 ],
+  [ true, 62 ],
+  [ false, 62 ],
 ];
 
 function makeArray(array) {
-    const out = [];
-    for (let j = 0; j < array.size(); j++) {
-        out.push(array.get(j));
-    }
-    return out;
+  const out = [];
+  for (let j = 0; j < array.size(); j++) {
+    out.push(array.get(j));
+  }
+  return out;
 }
 
 function writeScoreToRenderer(renderer, notes) {
-    renderer.clear();
-    notes.forEach(n => {
-        const [
-            dt, on, pitch,
-            velocity = on ? DEF_ON_VELOCITY : DEF_OFF_VELOCITY,
-            channel = DEF_CHANNEL
-        ] = n;
-        renderer.pushEvent(dt, note(on, pitch, velocity, channel));
-    });
-    renderer.finalize();
+  renderer.clear();
+  notes.forEach(n => {
+    const [
+      dt, on, pitch,
+      velocity = on ? DEF_ON_VELOCITY : DEF_OFF_VELOCITY,
+      channel = DEF_CHANNEL
+    ] = n;
+    renderer.pushEvent(dt, note(on, pitch, velocity, channel));
+  });
+  renderer.finalize();
 }
 
 function getRendererEventSets(renderer) {
-    const out = [];
-    while (renderer.hasEvents(true)) {
-        const eventsSet = makeArray(renderer.pullEvents());
-        out.push(eventsSet);
-    }
-    return out;
+  const out = [];
+  while (renderer.hasEvents(true)) {
+    const eventsSet = makeArray(renderer.pullEvents());
+    out.push(eventsSet);
+  }
+  return out;
 }
 
 function getRendererOutput(renderer, commands) {
-    const out = [];
-    commands.forEach(c => {
-        const [
-            pressed, id,
-            velocity = pressed ? DEF_ON_VELOCITY : DEF_OFF_VELOCITY,
-            channel = DEF_CHANNEL
-        ] = c;
-        const eventSet = makeArray(
-            renderer.combine3(command(pressed, id, velocity, channel), true)
-        );
-        out.push(eventSet);
-    })
-    return out;
+  const out = [];
+  commands.forEach(c => {
+    const [
+      pressed, id,
+      velocity = pressed ? DEF_ON_VELOCITY : DEF_OFF_VELOCITY,
+      channel = DEF_CHANNEL
+    ] = c;
+    const events = makeArray(
+      renderer.render(command(pressed, id, velocity, channel))
+    );
+    out.push(events);
+  })
+  return out;
 }
 
 
-/* * * * * * * * * * * * * * * * * * TESTS * * * * * * * * * * * * * * * * * */
+// TESTS ///////////////////////////////////////////////////////////////////////
 
 MidifilePerformer.onRuntimeInitialized = function() {
-    const renderer = new MidifilePerformer.Renderer({
-        unmeet:             true,
-        complete:           false,
-        shiftMode:          MidifilePerformer.shiftMode.pitchAndChannel,
-        temporalResolution: 0,
-    });
+  const renderer = new MidifilePerformer.Performer({
+    unmeet:             true,
+    complete:           false,
+    shiftMode:          MidifilePerformer.shiftMode.pitchAndChannel,
+    temporalResolution: 0,
+  });
 
-    test('chord velocities', function(t) {
-        t.plan(1);
-        const score = [
-            [ 0, true, 60, 127 ],
-            [ 0, true, 64, 100 ],
-            [ 0, true, 67, 4 ],
-            [ 5, false, 60 ],
-            [ 0, false, 64 ],
-            [ 0, false, 67 ],
-        ];
-        function makeChordCommand(velocity) {
-            return [
-                [ true, 60, velocity ],
-                [ false, 60],
-            ];
-        }
+  test('chord velocities', function(t) {
+    t.plan(1);
+    const score = [
+      [ 0, true, 60, 127 ],
+      [ 0, true, 64, 100 ],
+      [ 0, true, 67, 4 ],
+      [ 5, false, 60 ],
+      [ 0, false, 64 ],
+      [ 0, false, 67 ],
+    ];
+    function makeChordCommand(velocity) {
+      return [
+        [ true, 60, velocity ],
+        [ false, 60 ],
+      ];
+    }
 
-        renderer.setChordRenderingStrategy(
-            //MidifilePerformer.chordStrategy.sameForAll
-            //MidifilePerformer.chordStrategy.clippedScaledFromMean
-            //MidifilePerformer.chordStrategy.adjustedScaledFromMean
-            MidifilePerformer.chordStrategy.clippedScaledFromMax
-        );
+    renderer.setChordRenderingStrategy(
+      //MidifilePerformer.chordStrategy.sameForAll
+      //MidifilePerformer.chordStrategy.clippedScaledFromMean
+      //MidifilePerformer.chordStrategy.adjustedScaledFromMean
+      MidifilePerformer.chordStrategy.clippedScaledFromMax
+    );
 
-        let output;
-        writeScoreToRenderer(renderer, score);
-        const model = renderer.getPartition();
-        
-        output = getRendererOutput(renderer, makeChordCommand(127));
-        console.log(output);
+    let output;
+    writeScoreToRenderer(renderer, score);
+    const model = renderer.getChronology();
+    
+    output = getRendererOutput(renderer, makeChordCommand(127));
+    // console.log(output);
 
-        renderer.setPartition(model);
-        output = getRendererOutput(renderer, makeChordCommand(64));
-        console.log(output);
+    renderer.setChronology(model);
+    output = getRendererOutput(renderer, makeChordCommand(64));
+    // console.log(output);
 
-        renderer.setPartition(model);
-        output = getRendererOutput(renderer, makeChordCommand(1));
-        console.log(output);
+    renderer.setChronology(model);
+    output = getRendererOutput(renderer, makeChordCommand(1));
+    // console.log(output);
 
-        t.equal(true, true);
-    });
+    t.equal(true, true);
+  });
 
-    test('overlap', function(t) {
-        t.plan(1);
-        // writeScoreToRenderer(renderer, normalScore);
-        // const output = getRendererOutput(renderer, longCommandSeries);
-        // const output = getRendererEventSets(renderer);
-        // console.log(output);
-        t.equal(true, true);
-    });
+  test('overlap', function(t) {
+    t.plan(1);
+    // writeScoreToRenderer(renderer, normalScore);
+    // const output = getRendererOutput(renderer, longCommandSeries);
+    // const output = getRendererEventSets(renderer);
+    // console.log(output);
+    t.equal(true, true);
+  });
 
-    test('begins', function(t) {
-        t.plan(1);
-        t.equal(false, false);
-    });
+  test('begins', function(t) {
+    t.plan(1);
+    t.equal(false, false);
+  });
+
+  test('ill-formed score', function(t) {
+    t.plan(1);
+    const score = [
+      [ 0,  false,  60 ], // end events before first start event should be discarded
+      [ 10, true,   61, 127 ],
+      [ 10, true,   62, 127 ],
+      [ 10, false,  62 ],
+      [ 10, false,  61 ],
+      [ 10, false,  60 ],
+    ];
+
+    const commands = [
+      [ true,   60 ],
+      [ true,   61 ],
+      [ false,  61 ],
+      [ false,  60 ],
+    ];
+
+    let output;
+    writeScoreToRenderer(renderer, score);
+    output = getRendererOutput(renderer, commands);
+    console.log(output);
+    t.equal(true, true);
+  }); 
 };
